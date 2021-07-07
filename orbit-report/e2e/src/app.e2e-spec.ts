fe7952be707415c9aa35ea245c24bbff80ea9448
Katie Farris <klynnfarris@gmail.com>
2021-07-07 15:00:24 -0500

    final changes

--- orbit-report/e2e/src/app.e2e-spec.ts
+++ orbit-report/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to orbit-report!');
+    expect(page.getTitleText()).toEqual('orbit-report app is running!');
   });
 
   afterEach(async () => {
